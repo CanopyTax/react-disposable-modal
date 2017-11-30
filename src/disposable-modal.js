4246c1c75655e8a871e1ae934529fedc93d8e5bf
Bret Little <bret.little@gmail.com>
2017-11-30 16:28:15 -0700

    Fix things again

--- src/disposable-modal.js
+++ src/disposable-modal.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Cancelable from 'react-disposable-decorator';
-import mountComponent from 'disposable-component';
+import React from "react";
+import ReactDOM from "react-dom";
+import Cancelable from "react-disposable-decorator";
+import mountComponent from "disposable-component";
 
 class _Modal extends React.Component {
   componentDidMount() {
     const children = this.props.children;
 
-    this.cancelWhenUnmounted(createCancelableModal(function Portal() {
-      return children;
-    }, this.props));
+    this.props.cancelWhenUnmounted(
+      createCancelableModal(function Portal() {
+        return children;
+      }, this.props).subscribe(() => {})
+    );
   }
 
   render() {
@@ -22,16 +24,24 @@ export const Modal = Cancelable(_Modal);
 export function createCancelableModal(El, props = {}) {
   let el;
 
-  return mountComponent((onNext, onCompleted, onError) => {
-		el = document.createElement("div");
-		document.body.appendChild(el);
+  return mountComponent(
+    (onNext, onCompleted, onError) => {
+      el = document.createElement("div");
+      document.body.appendChild(el);
 
-		ReactDOM.render(
-			<El {...props} close={onCompleted} onNext={onNext} />,
-			el
-		);
-  }, () => {
-		ReactDOM.unmountComponentAtNode(el);
-		el.parentNode.removeChild(el);
-  })
+      ReactDOM.render(
+        <El
+          {...props}
+          onCompleted={onCompleted}
+          onNext={onNext}
+          onError={onError}
+        />,
+        el
+      );
+    },
+    () => {
+      ReactDOM.unmountComponentAtNode(el);
+      el.parentNode.removeChild(el);
+    }
+  );
 }
